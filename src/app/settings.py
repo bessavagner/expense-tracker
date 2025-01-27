5b52b2dab2c92b8a2ebdfa0945ee679c6108b636
bessavagner <bessavagner@gmail.com>
2025-01-27 17:30:55 -0300

    fix: add db host to settings

--- src/app/settings.py
+++ src/app/settings.py
@@ -109,7 +109,7 @@ if not PRODUCTION:
             'NAME': env('POSTGRES_DB'),
             'USER': env('POSTGRES_USER'),
             'PASSWORD': env('POSTGRES_PASSWORD'),
-            'HOST': 'localhost',
+            'HOST': env('POSTGRES_HOST'),
             'PORT': env('POSTGRES_PORT'),
         }
     }
