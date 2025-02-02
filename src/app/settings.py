92911b926c9fe6db60330d7fe7f379f64789a95a
bessavagner <bessavagner@gmail.com>
2025-02-02 20:32:17 -0300

    feat: improve emails styles

--- src/app/settings.py
+++ src/app/settings.py
@@ -136,6 +136,8 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            'debug': DEBUG,  # Enable template debugging
+            'string_if_invalid': 'INVALID EXPRESSION: %s',  # Show invalid template variables
         },
     },
 ]
