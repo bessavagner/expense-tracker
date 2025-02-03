64288b8e1fc94cb9803fb4977eacd22f264271ba
bessavagner <bessavagner@gmail.com>
2025-02-03 16:24:59 -0300

    fix: remove custom form from singup view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CustomLoginView, SignupView, PasswordChangeView, EmailView
+from .views import LoginView, SignupView, PasswordChangeView, EmailView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('email/', EmailView.as_view(), name='email'),
     path('password_change/', PasswordChangeView.as_view(), name='password_change'),
