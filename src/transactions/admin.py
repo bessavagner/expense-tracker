8e86be62041eda10742845a3ba0f7b94def66a06
bessavagner <bessavagner@gmail.com>
2025-02-03 20:54:32 -0300

    feat: add admin

--- src/transactions/admin.py
+++ src/transactions/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Currency
+from .models import Currency, Category, CategoryUser, Transaction
 
 admin.site.register(Currency)
+admin.site.register(Category)
+admin.site.register(CategoryUser)
+admin.site.register(Transaction)
